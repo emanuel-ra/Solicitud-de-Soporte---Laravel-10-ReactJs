decd97bf0f3d64b43980e2a61982ece8c0ef9822
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-08-04 18:26:22 -0600

    Laravel + Ractjs project created

---
+++ resources/js/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function app() {
+  return (
+    <>
+        <div>app</div>
+        <button className='btn btn-info'>hello</button>
+    </>
+  )
+}
+
+export default app
